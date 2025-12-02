0747d3a627aa2602e6c3d9616532f955325dd655
Timothy Obote <gortimothyobote@gmail.com>
2025-12-02 15:25:52 +0300

    Sync

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ const io = new Server(server, { cors: { origin: "*" } });
 // Database connection
 const pool = mysql
   .createPool({
-    host: "localhost",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "peerbridge",
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
